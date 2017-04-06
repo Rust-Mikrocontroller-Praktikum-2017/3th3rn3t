b7957748c55bc88f9d16de8b957ad4a0054b5664
Julian Herr <mail@julianherr.de>
2017-04-06 14:20:23 +0200

    Fix rng.rs. Does not produce random numbers

--- src/rng.rs
+++ src/rng.rs
@@ -2,10 +2,10 @@ use core::ptr;
 use core::result::Result;
 use bit_field::BitField;
 
-const rng_base_addr: u32 = 0x5006_0800;
-const rng_cr: u32 = rng_base_addr + 0x0;
-const rng_status: u32 = rng_base_addr + 0x4;
-const rng_data: u32 = rng_base_addr + 0x8;
+const RNG_BASE_ADDR: u32 = 0x5006_0800;
+const RNG_CR: u32 = RNG_BASE_ADDR + 0x0;
+const RNG_STATUS: u32 = RNG_BASE_ADDR + 0x4;
+const RNG_DATA: u32 = RNG_BASE_ADDR + 0x8;
 
 pub struct Rng(u32, u32);
 
@@ -13,42 +13,30 @@ pub struct Rng(u32, u32);
 pub enum ErrorType {
     CECS,
     SECS,
-    AlreadyEnabled
+    AlreadyEnabled,
+    NotReady
 }
 
 fn disable_cr () {
 
-    let mut bits = unsafe { ptr::read_volatile(rng_cr as *mut u32) };
+    let mut bits = unsafe { ptr::read_volatile(RNG_CR as *mut u32) };
     bits.set_bit(2, false);
     bits.set_bit(3, false);
 
-    unsafe { ptr::write_volatile(rng_cr as *mut u32, bits) };
+    unsafe { ptr::write_volatile(RNG_CR as *mut u32, bits) };
 }
 
 fn enable_cr () {
 
-    // clear SEIS bit
-    // let mut data = unsafe { ptr::read_volatile(rng_status as *mut u32) };
-    // let mut d = data.clone();
-    // d.set_bit(6, false);
-
-    // unsafe { ptr::write_volatile(rng_status as *mut u32, d)};
-
-    let mut bits = unsafe { ptr::read_volatile(rng_cr as *mut u32) };
+    let mut bits = 0;
     bits.set_bit(2, true);
-    bits.set_bit(3, false);
 
-    unsafe { ptr::write_volatile(rng_cr as *mut u32, bits) };
-}
-
-pub fn tick(rng: &mut Rng) {
-
-
+    unsafe { ptr::write_volatile(RNG_CR as *mut u32, bits) };
 }
 
 pub fn enable() -> Result<Rng, ErrorType> {
 
-    let reg_content = unsafe { ptr::read_volatile(rng_cr as *mut u32) };
+    let reg_content = unsafe { ptr::read_volatile(RNG_CR as *mut u32) };
     if reg_content.get_bit(2) {
         return Err(ErrorType::AlreadyEnabled);
     }
@@ -61,20 +49,22 @@ pub fn enable() -> Result<Rng, ErrorType> {
 }
 
 impl Rng {
-    pub fn poll_and_get(&mut self) -> Result<Option<u32>, ErrorType> {
+    pub fn poll_and_get(&mut self) -> Result<u32, ErrorType> {
 
-        let content = unsafe { ptr::read_volatile(rng_status as *mut u32) };
+        let status = unsafe { ptr::read_volatile(RNG_STATUS as *mut u32) };
 
-        if !content.get_bit(1) {
-            if !content.get_bit(2) {
-                if content.get_bit(0) {
-                    let data = unsafe { ptr::read_volatile(rng_data as *mut u32) };
+        if !status.get_bit(1) {
+            if !status.get_bit(2) {
+                if status.get_bit(0) {
+                    let data = unsafe { ptr::read_volatile(RNG_DATA as *mut u32) };
                     if data != self.0 {
                         self.0 = data;
-                        return Ok(Some(data));
+                        self.1 = 0;
+                        return Ok(data);
                     }
                 }
             } else {
+                disable_cr();
                 enable_cr(); // recommended by manual
                 return Err(ErrorType::SECS);
             }
@@ -83,17 +73,45 @@ impl Rng {
         }
 
         self.1 += 1;
-        if self.1 > 10 {
+        if self.1 > 40 {
+            println!("RNG reset");
             disable_cr();
             enable_cr();
             self.1 = 0;
         }
-
         // data was not ready, try again!
-        Ok(None)
+        Err(ErrorType::NotReady)
     }
 
     pub fn disable(self) {
-        unsafe { ptr::write_volatile(rng_cr as *mut u32, 0x0) };
+        unsafe { ptr::write_volatile(RNG_CR as *mut u32, 0x0) };
     }
 }
+
+
+pub fn tick(rng: &mut Rng) {
+    match rng.poll_and_get() {
+
+        Ok(number) => {
+            println!("Random number received {}", number);
+        }
+        Err(e) => {
+            match e {
+                ErrorType::CECS => {
+                    println!("CECS error received");
+                }
+                ErrorType::SECS => {
+                    println!("SECS error received");
+                }
+                ErrorType::AlreadyEnabled => {
+                    unreachable!();
+                }
+                ErrorType::NotReady => {
+                    println!("No Random Number Ready Yet");
+                }
+            }
+        }
+    }
+}
+
+
